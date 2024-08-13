ceda81c6141fc0214789474ee9fee47e511a30b1
CrisAlex <cads.14alex@outlook.com>
2024-08-13 10:45:17 -0500

    First commit

--- Program.cs
+++ Program.cs
@@ -8,20 +8,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>();
 
-builder.Services.AddDbContext<AppDbContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
-
-builder.Services.AddCors();
+builder.Services.AddDbContext<AppDbContext>(opts =>
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 var app = builder.Build();
 
-app.MapPost("/", async ([FromBody] RegisterUserDto dto, UserManager<User> userManager) =>
+app.MapPost("/register", async ([FromBody] RegisterUserDto dto, UserManager<User> userManager) =>
 {
     User user = new() { Email = dto.Email, UserName = dto.UserName };
-    var result = await userManager.CreateAsync(user, dto.Password);
 
-    return result.Succeeded ? Results.Ok(user.Id) : Results.BadRequest(result.Errors);
+    var result = await userManager.CreateAsync(user, dto.Password);
+    if (!result.Succeeded) return Results.BadRequest(result.Errors);
+
+    return Results.Created();
 });
 
-app.UseCors(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.MapPost("/login", async ([FromBody] LoginDto dto, UserManager<User> userManager, SignInManager<User> signInManager) =>
+{
+    var user = await userManager.FindByEmailAsync(dto.Email);
+    if (user is null) return Results.NotFound("User not found.");
+
+    var confirmedEmail = await userManager.IsEmailConfirmedAsync(user);
+    if (!confirmedEmail) return Results.BadRequest("Email not confirmed.");
+
+    var result = await signInManager.PasswordSignInAsync(user, dto.Password, false, false);
+    return result.Succeeded ? Results.Ok("Successful login!") : Results.BadRequest("Failed sign-in.");
+});
 
 app.Run();
--- RegisterUserDto.cs
+++ RegisterUserDto.cs
@@ -6,4 +6,10 @@
         public string Email { get; set; }
         public string Password { get; set; }
     }
+
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
 }
