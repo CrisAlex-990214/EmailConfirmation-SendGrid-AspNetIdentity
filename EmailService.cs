779b66a3e64e581f7efffe3975a2526b7b5d0196
CrisAlex <cads.14alex@outlook.com>
2024-08-15 21:02:32 -0500

    Final commit

---
+++ EmailService.cs
@@ -0,0 +1,21 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace SignupIdentity
+{
+    public class EmailService(IConfiguration configuration)
+    {
+        public async Task SendEmailConfirmation(string email, string confirmationLink)
+        {
+            var client = new SendGridClient(configuration["SendGridApiKey"]);
+            var from = new EmailAddress("YOUR_SENDGRID_EMAIL");
+            var subject = "Account Confirmation";
+            var to = new EmailAddress(email);
+            var plainTextContent = confirmationLink;
+            var htmlContent = string.Empty;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
 using SignupIdentity;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIdentity<User, IdentityRole>()
-    .AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<User, IdentityRole>(opts => opts.SignIn.RequireConfirmedEmail = true)
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddDbContext<AppDbContext>(opts =>
     opts.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
@@ -19,6 +22,12 @@ app.MapPost("/register", async ([FromBody] RegisterUserDto dto, UserManager<User
 
     var result = await userManager.CreateAsync(user, dto.Password);
     if (!result.Succeeded) return Results.BadRequest(result.Errors);
+
+    var token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+    var code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+
+    var confirmationLink = $"https://localhost:7266/confirm-email?email={user.Email}&code={code}";
+    await new EmailService(builder.Configuration).SendEmailConfirmation(user.Email, confirmationLink);
 
     return Results.Created();
 });
@@ -33,6 +42,17 @@ app.MapPost("/login", async ([FromBody] LoginDto dto, UserManager<User> userMana
 
     var result = await signInManager.PasswordSignInAsync(user, dto.Password, false, false);
     return result.Succeeded ? Results.Ok("Successful login!") : Results.BadRequest("Failed sign-in.");
+});
+
+app.MapGet("/confirm-email", async ([FromQuery] string email, string code, UserManager<User> userManager) =>
+{
+    var user = await userManager.FindByEmailAsync(email);
+    if (user is null) return Results.NotFound("User not found.");
+
+    var token = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+    var result = await userManager.ConfirmEmailAsync(user, token);
+
+    return result.Succeeded ? Results.Ok("Confirmed!") : Results.BadRequest(result.Errors);
 });
 
 app.Run();
